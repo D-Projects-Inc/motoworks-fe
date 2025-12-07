7f72b6be3162b36d6ca2b8190ffe923fc3401b74
johan-khanye <johan.khanye@gmail.com>
2025-12-07 05:26:41 +0200

    Use env vars to conn. to diff. BE dpndng on env.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,8 @@ import sitemap from '@astrojs/sitemap';
 import preact from "@astrojs/preact";
 import icon from 'astro-icon';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://ejahman.netlify.app/",
-  integrations: [
-      icon(),
-      sitemap(), preact()
-	],
-  adapter: netlify(),
+  integrations: [icon(), sitemap(), preact()],
 });
--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,13 +1,10 @@
 console.log("API_URL:", import.meta.env.PUBLIC_API_URL);
-
 const BASE_BACKEND_URL = import.meta.env.PUBLIC_API_URL;
-
 const form = document.getElementById('contactForm');
 const messageBox = document.getElementById('formMessage');
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault(); // prevent normal form submit
-
   const formData = new FormData(form);
 
   // Send form data to Django backend
@@ -16,16 +13,12 @@ form.addEventListener('submit', async (e) => {
       method: 'POST',
       body: formData,
     });
-
     if (!res.ok) throw new Error('Network response was not ok');
-
     // Show success message
     messageBox.style.display = 'block';
     messageBox.style.color = 'green';
     messageBox.textContent = "Thanks! Your message has been sent. We'll get back to you soon.";
-
     form.reset(); // clear the form
-
   } catch (err) {
     // Show error
     messageBox.style.display = 'block';
